91309a51706cc5d2858ee88874832df43d360df7
SandraBender <sandraos15@ru.is>
2016-10-12 11:08:39 +0000

    Tók aðeins til í kóðanum

--- src/main/java/is/ru/stringcalculator/calculator.java
+++ src/main/java/is/ru/stringcalculator/calculator.java
@@ -13,33 +13,36 @@ public class calculator {
 
 		else if (text.contains(",") || text.contains("\n")) {
 			String[] numbers = text.split(",|\n");
-
-			int ret = 0;
-			ArrayList<Integer> negativeNumb = new ArrayList<Integer>();
-
-			for(Integer i = 0; i < numbers.length; i++){
-				int number = Integer.parseInt(numbers[i]);
-
-				if (number < 0) {
-					negativeNumb.add(number);
-				}
-				if (number > 1000) {
-					continue;
-				}
-
-				else{
-			 		ret += Integer.parseInt(numbers[i]);
-			 	}
-			}
-			if (negativeNumb.size() > 0) {
-				throw new RuntimeException("Negatives not allowed: " + negativeNumb);
-			}
-			return ret;
+			return stringNumbers(numbers);
 		}
 
 		else{
 			int ret = Integer.parseInt(text);
 			return ret;
 		}
+	}
+
+	public static int stringNumbers(String[] numbers){
+		int ret = 0;
+		ArrayList<Integer> negativeNumb = new ArrayList<Integer>();
+
+		for(Integer i = 0; i < numbers.length; i++){
+			int number = Integer.parseInt(numbers[i]);
+
+			if (number < 0) {
+				negativeNumb.add(number);
+			}
+			if (number > 1000) {
+				continue;
+			}
+
+			else{
+			 	ret += Integer.parseInt(numbers[i]);
+			}
+		}
+		if (negativeNumb.size() > 0) {
+			throw new RuntimeException("Negatives not allowed: " + negativeNumb);
+		}
+		return ret;
 	}
 }
